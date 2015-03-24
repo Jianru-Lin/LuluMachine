7fc03da1f5b552c53fe7041ef086082c858036f1
Jianru Lin <jianru.lin@gmail.com>
2015-03-24 14:19:29 +0800

    input-area

--- public/tokenizer/main.js
+++ public/tokenizer/main.js
@@ -1,14 +1,22 @@
 ;$(function() {
 	var editor
+	var input
 
 	ace.require("ace/ext/language_tools")
+	
 	editor = ace.edit("editor")
-	editor.setTheme("ace/theme/dawn")
+	editor.setTheme("ace/theme/ambiance")
 	editor.getSession().setMode("ace/mode/javascript")
 	editor.setValue('')
-	editor.setOptions({
-		enableBasicAutocompletion: true,
-		enableSnippets: true,
-		enableLiveAutocompletion: true
-	})
+	// editor.setOptions({
+	// 	enableBasicAutocompletion: true,
+	// 	enableSnippets: true,
+	// 	enableLiveAutocompletion: true
+	// })
+
+	input = ace.edit("input")
+	input.setTheme("ace/theme/dawn")
+	input.getSession().setMode("ace/mode/text")
+	input.setValue('')
+
 });
