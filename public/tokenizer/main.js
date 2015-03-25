f7f5700eb042c79187cbe306d56e60ac10e5f37c
Jianru Lin <jianru.lin@gmail.com>
2015-03-25 15:42:55 +0800

    player

--- public/tokenizer/main.js
+++ public/tokenizer/main.js
@@ -42,14 +42,116 @@ $(function() {
 })
 
 $(function() {
+
+	var player = {
+
+		// tokenizer management
+
+		tokenizerCache: {},
+		
+		defTokenizer: function(name, def) {
+			if (typeof name !== 'string' ||
+				typeof def !== 'function') {
+				return
+			}
+			this.tokenizerCache[name] = def
+		},
+		
+		clearTokenizerCache: function() {
+			this.tokenizerCache = {}
+		},
+
+		// play loop
+
+		_loopSpan: 1000,
+		_loopHandle: undefined,
+		_src: undefined,
+		_txt: undefined,
+		_nextPos: 0,
+
+		get src() {
+			return _src
+		},
+
+		set src(v) {
+			this._src = v
+			var fun = new Function('tokenizer', v)
+			fun(this.defTokenizer.bind(this))
+		},
+
+		get txt() {
+			return this._txt
+		},
+
+		set txt(v) {
+			this._txt = v
+		},
+
+		get nextPos() {
+			return this._nextPos
+		},
+
+		set nextPos(v) {
+			this._nextPos = v
+		},
+
+		reset: function() {
+			// clear tokenizer cache
+			this.clearTokenizerCache()
+		},
+
+		play: function() {
+			// start loop
+			this._startLoop()
+		},
+
+		pause: function() {
+			this._stopLoop()
+		},
+
+		_startLoop: function() {
+			if (this._loopHandle) {
+				return
+			}
+			this._loopHandle = setInterval(this._step.bind(this), this._loopSpan)
+		},
+
+		_stopLoop: function() {
+			if (!this._loopHandle) {
+				return
+			}
+			clearInterval(this._loopSpan)
+			this._loopHandle = undefined
+		},
+
+		_step: function() {
+			console.log('step ' + new Date())
+		}
+	}
+
 	var controlbar = window.controlbar = new Vue({
 		el: '.controlbar',
 		methods: {
 			onPlay: function(e) {
 				var src = srcEditor.getValue()
+				if (!src) {
+					alert('引擎源代码为空！ Engine is empty.')
+					return
+				}
+
 				var txt = txtEditor.getValue()
-				alert(src)
-				alert(txt)
+				if (!txt) {
+					alert('测试输入为空！Text is empty.')
+					return
+				}
+
+				player.src = src
+				player.txt = txt
+				// notice: dont' change the nextPos
+				player.play()
+			},
+			onPause: function(e) {
+				alert('pause')
 			}
 		}
 	})
